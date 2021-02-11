462b8e75a4c62c4256d64a679983012131379237
Prajjwal Singh <prajjwal875.com>
2021-02-12 00:14:19 +0530

    setu

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require("http");
+const express = require("express");
+
+const app = express();
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+const io = socketio(server);
+io.on("connection", (socket) => {
+  console.log("Connected with socket id=", socket.id);
+});
+app.use("/", express.static(__dirname + "/public"));
+
+server.listen(3000, () => {
+  console.log("Started on http://localhost:3000");
+});
